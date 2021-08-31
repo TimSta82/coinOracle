bd5cafcfd785608044d3b586cd5658dcf6664ca9
tim.staats@bornholdtlee.de <tim.staats@bornholdtlee.de>
2021-08-31 23:07:48 +0200

    motion_layout: Implement filter options in UI

--- app/src/main/java/de/timbo/coinOracle/ui/trading_overview/TradingOverviewFragment.kt
+++ app/src/main/java/de/timbo/coinOracle/ui/trading_overview/TradingOverviewFragment.kt
@@ -8,6 +8,7 @@ import de.timbo.coinOracle.R
 import de.timbo.coinOracle.database.model.TradeEntity
 import de.timbo.coinOracle.databinding.FragmentTradingOverviewBinding
 import de.timbo.coinOracle.ui.BaseFragment
+import de.timbo.coinOracle.utils.Logger
 import de.timbo.coinOracle.utils.viewBinding
 
 class TradingOverviewFragment : BaseFragment(R.layout.fragment_trading_overview) {
@@ -20,10 +21,53 @@ class TradingOverviewFragment : BaseFragment(R.layout.fragment_trading_overview)
 
         setObservers()
         // TODO implement switch
+        setRadioButtonClicklisteners()
+    }
+
+    private fun setRadioButtonClicklisteners() {
+        binding.allRb.setOnCheckedChangeListener { _, isChecked ->
+            binding.allRb.isChecked = isChecked
+            if (isChecked) {
+                binding.purchaseRb.isChecked = isChecked.not()
+                binding.soldRb.isChecked = isChecked.not()
+            }
+            viewModel.applyFilter(FilterOption.ALL)
+        }
+        binding.purchaseRb.setOnCheckedChangeListener { _, isChecked ->
+            binding.purchaseRb.isChecked = isChecked
+            if (isChecked) {
+                binding.allRb.isChecked = isChecked.not()
+                binding.soldRb.isChecked = isChecked.not()
+            }
+            viewModel.applyFilter(FilterOption.PURCHASED)
+        }
+        binding.soldRb.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.allRb.isChecked = isChecked.not()
+                binding.purchaseRb.isChecked = isChecked.not()
+            }
+            binding.soldRb.isChecked = isChecked
+            viewModel.applyFilter(FilterOption.SOLD)
+        }
+        binding.ascRb.setOnCheckedChangeListener { _, isChecked ->
+            binding.ascRb.isChecked = isChecked
+            if (isChecked) binding.descRb.isChecked = isChecked.not()
+            viewModel.applyFilter(FilterOption.ASC)
+        }
+        binding.descRb.setOnCheckedChangeListener { _, isChecked ->
+            binding.descRb.isChecked = isChecked
+            if (isChecked) binding.ascRb.isChecked = isChecked.not()
+            viewModel.applyFilter(FilterOption.DESC)
+        }
     }
 
     private fun setObservers() {
         viewModel.trades.observe(viewLifecycleOwner) { trades -> setData(trades) }
+        viewModel.filterOptions.observe(viewLifecycleOwner, ::setFilter)
+    }
+
+    private fun setFilter(list: List<FilterOption>) {
+        Logger.debug("filterList: $list")
     }
 
     private fun setData(trades: List<TradeEntity>) {
--- app/src/main/java/de/timbo/coinOracle/ui/trading_overview/TradingOverviewViewModel.kt
+++ app/src/main/java/de/timbo/coinOracle/ui/trading_overview/TradingOverviewViewModel.kt
@@ -1,9 +1,6 @@
 package de.timbo.coinOracle.ui.trading_overview
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import de.timbo.coinOracle.database.model.TradeEntity
 import de.timbo.coinOracle.usecases.WatchTradesUseCase
 import org.koin.core.component.KoinComponent
@@ -14,4 +11,18 @@ class TradingOverviewViewModel : ViewModel(), KoinComponent {
     private val watchTradesUseCase by inject<WatchTradesUseCase>()
 
     val trades: LiveData<List<TradeEntity>> = watchTradesUseCase.call().asLiveData(viewModelScope.coroutineContext)
+
+    private val _filterOptions = MutableLiveData<List<FilterOption>>()
+    val filterOptions : LiveData<List<FilterOption>> = _filterOptions
+
+    var filterList = mutableListOf<FilterOption>(FilterOption.ALL, FilterOption.ASC)
+
+    fun applyFilter(filterOption: FilterOption) {
+        if (filterList.contains(filterOption)) filterList.remove(filterOption) else filterList.add(filterOption)
+        _filterOptions.value = filterList
+    }
+}
+
+enum class FilterOption {
+    ALL, PURCHASED, SOLD, ASC, DESC
 }
